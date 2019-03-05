1f5875df2164985cbcef96c6b217e198f246ebcc
BlakeBeyond <blake.lezenski@gmail.com>
2019-03-05 18:49:50 +0800

    inheritance_commit

--- week_02/11_inheritance/01_class_attributes.py
+++ week_02/11_inheritance/01_class_attributes.py
@@ -10,19 +10,38 @@ Flush out the classes below with the following:
 '''
 
 class Class1:
-
+    def __init__(self, x):
+        self.x = x
     # define an __init__() method that sets an attribute x
-    pass
+    #pass
 
-class Class2:
+
+class Class2(Class1):
+    def __init__(self, x, y):
+        super().__init__(x)
+        self.y = y
 
     # define an __init__() method that sets an attribute y and calls the __init__() method of its parent
-    pass
+    #pass
 
-class Class3:
+
+class Class3(Class2):
+    def __init__(self, x, y, z):
+        super().__init__(x, y)
+        self.z = z
+
 
     # define an __init__() method that sets an attribute z and calls the __init__() method of its parent
-    pass
+    #pass
 
 
 # create an object of each class and print each of its attributes
+object1 = Class1("mama")
+object2 = Class2("mama", "tata")
+object3 = Class3("mama", "tata", "nana")
+
+print(object1.x)
+print(object2.y, object2.x)
+print(object3.z, object3.x, object3.y)
+
+
--- week_02/11_inheritance/02_dog.py
+++ week_02/11_inheritance/02_dog.py
@@ -8,3 +8,26 @@ Create and object of this class and demonstrate it's functionality.
 
 '''
 
+
+class Dog:
+
+    def __init__(self, name, color, age, is_hungry=False, percent_full=100):
+        self.name = name
+        self.color = color
+        self.age = age
+        self.is_hungry = is_hungry
+        self.percent_full = percent_full
+
+
+class Pugs(Dog):
+
+    def __init__(self, name, color, age, is_hungry=False, percent_full=100, ears="Floppy"):
+        super().__init__(name, color, age, is_hungry, percent_full)
+        self.ears = ears
+
+
+puggy = Pugs("Rex", "brown", 8, True, 40, "Spiky")
+
+print(puggy.name, puggy.ears)
+
+
