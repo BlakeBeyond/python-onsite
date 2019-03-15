60d2693b50f815a2c1bf26cc8949e11100d713c6
BlakeBeyond <blake.lezenski@gmail.com>
2019-03-15 15:07:57 +0800

    web_scraping

--- week_04/web_scraping/01_your_page.py
+++ week_04/web_scraping/01_your_page.py
@@ -6,3 +6,17 @@ BONUS: extend your python program so that it reads your original HTML file
        and returns True if the HTML from the response is the same as the
        the contents of the original HTML file.
 '''
+
+import requests
+
+url = "https://blakebeyond.github.io/blakebeyond/"
+direct = 'file://localhost/Users/Blake/PycharmProjects/week2/gh-pages/index.html'
+res = requests.get(url)
+
+
+print(res.content)
+if res.content == direct:
+    print(True)
+else:
+    print(False)
+
--- week_04/web_scraping/02_wikipedia.py
+++ week_04/web_scraping/02_wikipedia.py
@@ -11,3 +11,46 @@ Collect:
 Store the information in a nicely formatted text file.
 
 '''
+
+from requests_html import HTMLSession
+
+url = "https://en.wikipedia.org/wiki/Zoroastrianism"
+session = HTMLSession()
+r = session.get(url)
+
+box = r.html.xpath('//*[@id="mw-content-text"]/div/table[1]', first=True)
+print(box.text)
+
+
+box1 = r.html.find('#mw-content-text > div > table.vertical-navbox.nowraplinks', first=True)
+print(box1.text)
+#div is a child element, vertcial navbox issa class
+
+two_img = r.html.find('img')
+print(two_img[0])
+print(two_img[1])
+
+
+ref_collection = r.html.find('p', containing='purpose in life', first=True)
+print(ref_collection.text)
+
+all_links = r.html.absolute_links
+print(all_links)
+
+all_titles = r.html.find('title')
+for i in all_titles:
+    print(i.text)
+
+i1 = two_img[0].attrs["src"]
+i2 = two_img[1].attrs["src"]
+t = all_titles[0].text
+with open("02_wikipedia.txt", 'a') as file_object:
+    file_object.write(f"{box.text}\n")
+    file_object.write(f"{box1.text}\n")
+    file_object.write(f"{i1}\n)")
+    file_object.write(f"{i2}\n")
+    file_object.write(f"{t}\n")
+    for i in all_links:
+        file_object.write(f"{i}\n")
+
+
